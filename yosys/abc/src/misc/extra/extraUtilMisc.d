27f8a299f116de9423372726591755cb796e5ce2
rafaeltp <rafaeltp@soe.ucsc.edu>
2017-06-14 13:21:58 -0700

    removed some .d files that were not cleaned properly

--- yosys/abc/src/misc/extra/extraUtilMisc.d
+++
@@ -1,3 +0,0 @@
-src/misc/extra/extraUtilMisc.d src/misc/extra/extraUtilMisc.o: src/misc/extra/extraUtilMisc.c src/misc/extra/extra.h \
-  src/misc/st/st.h src/misc/util/abc_global.h \
-  src/misc/util/abc_namespaces.h
