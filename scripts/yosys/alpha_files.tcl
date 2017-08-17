22df31236a90347b52d9951801e4be74f886c835
rafaeltp <rafaeltp@soe.ucsc.edu>
2017-08-17 16:46:38 -0700

    added scripts for yosys runs / cleaned up Makefile

---
+++ scripts/yosys/alpha_files.tcl
@@ -0,0 +1,10 @@
+
+read_verilog -sv ../../alpha/sys_defs.vh
+read_verilog -sv ../../alpha/ex_stage.v
+read_verilog -sv ../../alpha/id_stage.v
+read_verilog -sv ../../alpha/if_stage.v
+read_verilog -sv ../../alpha/mem_stage.v
+read_verilog -sv ../../alpha/pipeline.v
+read_verilog -sv ../../alpha/regfile.v
+read_verilog -sv ../../alpha/wb_stage.v
+
---
+++ scripts/yosys/dlx_files.tcl
@@ -0,0 +1,14 @@
+
+read_verilog -sv ../../dlx/globals.v
+read_verilog -sv ../../dlx/alu.v
+read_verilog -sv ../../dlx/alu_control.v
+read_verilog -sv ../../dlx/bypass_ex.v
+read_verilog -sv ../../dlx/bypass_id.v
+read_verilog -sv ../../dlx/cpu.v
+read_verilog -sv ../../dlx/decode.v
+read_verilog -sv ../../dlx/dlx_defs.v
+read_verilog -sv ../../dlx/ff.v
+read_verilog -sv ../../dlx/quick_compare.v
+read_verilog -sv ../../dlx/regfile.v
+read_verilog -sv ../../dlx/wb_control.v
+
